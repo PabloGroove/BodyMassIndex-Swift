0c7a2482bceca0b165b08feea6e5d3c4ab09845b
Pablo Groove <106037878+PabloGroove@users.noreply.github.com>
2022-07-11 19:37:42 +0200

    Create SwiftJam.swift

---
+++ SwiftJam.swift
@@ -0,0 +1,9 @@
+let tuneSquadPoints = 78
+let monstarsPoints = 77
+
+// Write your code below 🏀
+if tuneSquadPoints < monstarsPoints {
+  print("Monstars are the winners of today's game!")
+} else {
+  print("tuneSquad are the winners of today's game!")
+}
